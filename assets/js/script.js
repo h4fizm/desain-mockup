ecd9fefdea24557b3df804cd8f6cb88e5549c81e
revo.farel_dev <hafizfarel01@gmail.com>
2025-04-09 16:30:56 +0700

    modify style dashboard/home and daftar_mobil page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,18 @@
+// dropdown menu
+const btn = document.getElementById("dropdownButton");
+const menu = document.getElementById("dropdownMenu");
+
+btn.addEventListener("click", () => {
+  menu.classList.toggle("hidden");
+});
+
+// Optional: Klik di luar untuk menutup menu
+window.addEventListener("click", (e) => {
+  if (!btn.contains(e.target) && !menu.contains(e.target)) {
+    menu.classList.add("hidden");
+  }
+});
+
 // swiper js carousel section
 const swiper = new Swiper(".swiper", {
   slidesPerView: "auto",
